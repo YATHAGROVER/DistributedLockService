9c3141734be207379cbe3c389e7dcb6a112da230
yatha grover <groveryatha@gmail.com>
2026-07-11 11:28:39 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/project/DistributedLockService/DistributedLockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.DistributedLockService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedLockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedLockServiceApplication.class, args);
+	}
+
+}
