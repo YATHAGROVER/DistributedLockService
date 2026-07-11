bcc34cb6a3142335bc14c85c67ba0db105c83bcf
yatha grover <groveryatha@gmail.com>
2026-07-11 17:22:31 +0530

    Implement Lock entity and Lock repository

--- src/main/java/com/project/DistributedLockService/Entity/Lock.java
+++ src/main/java/com/project/DistributedLockService/Entity/Lock.java
@@ -1,4 +1,52 @@
 package com.project.DistributedLockService.Entity;
+import com.project.DistributedLockService.Enums.LockStatus;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "locks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Lock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "resource_name", nullable = false, unique = true)
+    private String resourceName;
+
+    @Column(name = "owner_id", nullable = false)
+    private String ownerId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LockStatus status;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+
+        if (status == null) {
+            status = LockStatus.ACTIVE;
+        }
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
