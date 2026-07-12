f31749187e8c24fe94fe9474aae758bf8d4767d4
gunjan <gunjanbhanwal@gmail.com>
2026-07-12 12:53:09 +0530

    adding dummy controllers

--- src/main/java/com/project/DistributedLockService/Controller/LockController.java
+++ src/main/java/com/project/DistributedLockService/Controller/LockController.java
@@ -1,4 +1,44 @@
 package com.project.DistributedLockService.Controller;
 
-public class LockController {
+import com.project.DistributedLockService.Dto.request.AcquireLockRequest;
+import com.project.DistributedLockService.Dto.request.HeartbeatRequest;
+import com.project.DistributedLockService.Dto.response.LockResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/locks")
+public class LockController{
+
+    @PostMapping
+    public ResponseEntity<String> acquireLock() {
+
+        return ResponseEntity.ok("Acquire Lock API Working");
+    }
+
+    @DeleteMapping("/{resourceName}")
+    public ResponseEntity<String> releaseLock(
+            @PathVariable String resourceName) {
+
+        return ResponseEntity.ok(
+                "Release Lock API Working for " + resourceName
+        );
+    }
+
+    @PostMapping("/heartbeat")
+    public ResponseEntity<String> heartbeat() {
+
+        return ResponseEntity.ok("Heartbeat API Working");
+    }
+
+    @GetMapping("/{resourceName}")
+    public ResponseEntity<String> getLockStatus(
+            @PathVariable String resourceName) {
+
+        return ResponseEntity.ok(
+                "Get Lock Status API Working for " + resourceName
+        );
+    }
+
 }
+
